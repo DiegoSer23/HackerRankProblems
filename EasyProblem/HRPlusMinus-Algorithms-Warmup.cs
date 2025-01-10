659c78c811718ffd5d50dcd1f3a2ca3903d32fbc
DiegoSer23 <50917522+DiegoSer23@users.noreply.github.com>
2025-01-09 20:45:59 -0600

    Create HRPlusMinus-Algorithms-Warmup.cs

---
+++ EasyProblem/HRPlusMinus-Algorithms-Warmup.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(int n, List<int> arr)
+    {
+        int positive = 0, negative = 0, zero = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (arr[i] > 0)
+                positive++;
+            else if (arr[i] < 0)
+                negative++;
+            else
+                zero++;
+        }
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(n, arr);
+    }
+}
