78e33495dffa297f2d70ec850806421a4a09877d
DiegoSer23 <50917522+DiegoSer23@users.noreply.github.com>
2025-01-09 20:28:24 -0600

    Create HRSimpleArraySum.cs

---
+++ HRSimpleArraySum.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        for (int i=0; i < ar.Count; i++)
+        {
+            sum += ar[i];
+        }
+        return sum;
+    }
+
+}
