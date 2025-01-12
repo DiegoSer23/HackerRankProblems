bd911569ea050419f354eb60bab095429f1e662f
DiegoSer23 <50917522+DiegoSer23@users.noreply.github.com>
2025-01-11 19:05:08 -0600

    Create HRBillDivision-Algorithms-Implementation.cs

---
+++ EasyProblem/HRBillDivision-Algorithms-Implementation.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<int> bill, int n, int k, int b)
+    {
+        int pay = 0, sum = 0, result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (k != i)
+                sum += bill[i];
+        }
+        pay = sum / 2;
+        result = b - pay;
+        if (result == 0)
+            Console.Write("Bon Appetit");
+        else
+            Console.Write(result);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, n, k, b);
+    }
+}
